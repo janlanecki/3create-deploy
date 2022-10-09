18707e04a85b41ab8b85d3f38731133740c7ee5a
janlanecki <jan.lanecki@gmail.com>
2022-10-09 03:18:18 +0200

    Fix, only compiler / _setTokenURI problems

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,30 +3,32 @@
 */
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
-// const { API_URL, PRIVATE_KEY } = process.env;
+const { API_URL, PRIVATE_KEY } = process.env;
+const privateKey = [`0x${PRIVATE_KEY}`]
+
 module.exports = {
    solidity: "0.8.4",
    defaultNetwork: "goerli",
    networks: {
       hardhat: {},
-      // goerli: {
-      //    url: API_URL,
-      //    accounts: [`0x${PRIVATE_KEY}`]
-      // },
-      // for mainnet
-      'optimism': {
-        url: "https://mainnet.optimism.io",
-        accounts: [privateKey1, privateKey2, ...]
+      goerli: {
+         url: API_URL,
+         accounts: privateKey
       },
+      // for mainnet
+      // 'optimism': {
+      //   url: "https://mainnet.optimism.io",
+      //   accounts: privateKey
+      // },
       // for testnet
       'optimism-goerli': {
         url: "https://goerli.optimism.io",
-        accounts: [privateKey1, privateKey2, ...]
+        accounts: privateKey
       },
       // for the local dev environment
       'optimism-local': {
         url: "http://localhost:8545",
-        accounts: [privateKey1, privateKey2, ...]
+        accounts: privateKey
       },
    },
 }
